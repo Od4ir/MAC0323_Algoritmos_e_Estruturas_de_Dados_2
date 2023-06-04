f16c29cb0fb0475dbcb0cfb25131f40b495676fd
Od4ir <101140991+Od4ir@users.noreply.github.com>
2023-06-04 20:01:58 -0300

    Função que realiza corte da string de dna

--- EP3/gerador.cpp
+++ EP3/gerador.cpp
@@ -15,9 +15,27 @@ string gera(int k) {
     return dna;
 }
 
+void corta_dna(int k, int t_min, int t_max, string &dna) {
+    int tam = dna.size();
+    int t_aux = t_max - t_min + 1;
+
+    for(int i = 0; i < k; i++) {
+        int t_sub = rand() % t_aux + t_min;
+        cout << "Tamanho: " << t_sub << endl;
+        // t_sub reprensenta o tamanho da substring que irei pegar;
+        int ind = rand() % (tam - t_sub);
+        cout << "índice inicial: " << ind << endl;
+        cout << i << ": " << dna.substr(ind, t_sub - ind) << endl;
+
+    }
+}
+
 int main() {
     int k; cin >> k;
     string dna = gera(k);
-
     cout << dna << endl;
+    cin >> k;
+    int tmax, tmin; cin >> tmax >> tmin;
+    corta_dna(k, tmin, tmax, dna);
+
 }
