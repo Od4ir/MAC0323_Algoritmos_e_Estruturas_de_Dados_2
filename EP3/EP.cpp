4133d17c0c450475f64f0141c3cd4ba06535c581
Od4ir <101140991+Od4ir@users.noreply.github.com>
2023-06-09 20:22:07 -0300

    Ajustes na interface do terminal

--- EP3/EP.cpp
+++ EP3/EP.cpp
@@ -6,13 +6,29 @@
 #include "testes.h"
 using namespace std;
 
+// Devolve o índice da primeira aparição da sequência aux
+// no vetor v;
+int pri(const vector<node>& v, const string aux) {
+
+}
+
+// Devolve o índice da última aparição da sequência aux
+// no vetor v;
+int ult(const vector<node>& v, const string aux) {
+
+}
+
+void add_arco(int u, int ini, int ult) {
+    // Vou precisar de um vetor de adjacências;
+}
+
 
 int main() {
     cout << " \n-----------/// BEM VINDX AO EP3 - Od4ir ///-----------\n\n";
 
     cout << " \n---------/// PARTE 1 - OBTENÇÃO DOS DADOS ///----------\n\n";
 
-    cout << " Vamos pegar um arquivo de teste para realizar as operações.\n Digite o nome do arquivo: \n >> ";
+    cout << " Vamos pegar um arquivo de teste para realizar as operações.\n \n Digite o nome do arquivo: \n >> ";
     string file_name;
     cin >> file_name;
 
@@ -22,14 +38,11 @@ int main() {
     if(arquivo.is_open()) {
         string dna, aux;
         getline(arquivo, dna);
-        cout << dna << endl;
         int copy, k;
         getline(arquivo, aux);
         copy = stoi(aux);
-        cout << copy << endl;
         getline(arquivo, aux);
         k = stoi(aux);
-        cout << k << endl;
 
         string linha;
         int i = 0;
@@ -38,10 +51,24 @@ int main() {
             node no(linha, i++);
             v.push_back(no);
         }
-    }
 
-    for(node n: v) {
-        cout << n.id << ": " << n.key << endl;
+        cout << "\n Deseja imprimir os dados? S/N: \n >> ";
+        char op; cin >> op;
+
+        if(op == 'S') {
+            cout << "\n DNA Original: " << dna << endl;
+            cout << " Número de cópias.............." << copy << endl;
+            cout << " Número de substrings.........." << k << endl;
+            cout << " Substrings: " << endl;
+            for(node n: v)
+                cout << "  " << n.id << ": " << n.key << endl;
+        }
+
+        cout << "\n\n---------/// PARTE 2 - MONTAGEM DOS ARCOS ///----------\n\n";
+
+    }
+    else {
+        cout << "Não foi possível abrir o arquivo. Tente novamente. \n";
     }
 
     return 0;
