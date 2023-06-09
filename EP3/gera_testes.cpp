cf2337072208c6403de29318665e366c980f7b81
Od4ir <101140991+Od4ir@users.noreply.github.com>
2023-06-08 23:11:02 -0300

    Programa que gera os testes pronto, junto com alguns testes

--- EP3/gera_testes.cpp
+++ EP3/gera_testes.cpp
@@ -2,9 +2,11 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <fstream>
 #include "EP.h"
 using namespace std;
 
+// Este programa tem como objetivo gerar os arquivos para realizar os testes do EP;
 
 bool compara_nodes(node& a, node& b) {
     return a.key < b.key;
@@ -51,10 +53,10 @@ node::node(string chave, int i) {
 
 
 int main() {
-    int n; 
+    int n;
     vertices v;
 
-    cout << " \n-----------/// BEM VINDX AO EP3 - Od4ir ///-----------\n\n";
+    cout << " \n-----------/// GERADOR DE TESTES EP3 - Od4ir ///-----------\n\n";
 
     cout << "Vamos começar!\n";
 
@@ -71,23 +73,49 @@ int main() {
     if(op == 'S')
         cout << dna << endl;
     
-    cout << "\n1.2) Vamos gerar substrings a partir dessa gerada.\n\n Digite o número de substrings que deseja gerar: \n >> ";
-    int k; cin >> k;
+    cout << "\n1.2) Vamos gerar substrings a partir dessa gerada.\n\n Digite o número de vezes que deseja copiar a original: \n >> ";
+    cin >> n;
 
     cout << "\n Digite o tamanho máximo e mínimo dessas strings: \n";
     int MIN, MAX; cout << " MIN >> "; cin >> MIN;
     cout << " MAX >> "; cin >> MAX;
 
+    cout << "\n Digite o parâmetro k mínimo para posterior montagem do grafo: \n >> ";
+    int k; cin >> k;
+
     cout << "\n1.3) Vamos gerar as substrings, guardá-las em um vetor.\n";
 
     // Gerando as substrings e guardando elas;
 
-    int tam_aux = MAX - MIN + 1;
-    for(int i = 0; i < k; i++) {
-        int tam = (rand() % tam_aux) + MIN;
-        node no(gera_substring(tam, dna), i + 1);
-        //cout << no.key << " guardado!" << endl;
-        v.push_back(no);
+    int copy = 0;
+    while(copy < n) { 
+        int tam_total = dna.size();
+        int tam = (rand() % (MAX - MIN + 1)) + MIN;
+        int c = tam_total / tam;
+
+        if(tam_total - tam * c < k && c > 1 && tam_total % tam != 0) {
+            for(int i = c; i > 0; i--) {
+                if(tam_total - tam * c >= k) {
+                    c = i;
+                    break;
+                }
+            }
+        }
+
+        if(c != 0 && (tam_total - tam * c >= k || tam_total % tam == 0)) {
+            int ind = 0;
+            for(int i = c; i > 0; i--) {
+                node no(dna.substr(ind, tam), 0);
+                v.push_back(no);
+                ind = ind + tam;
+            }
+            int aux = tam_total - tam * c;
+            if(aux != 0) {
+                node no(dna.substr(ind, aux), 0);
+                v.push_back(no);
+            }
+            copy++;
+        }
     }
 
     cout << "\n Deseja imprimir? S/N\n >> ";
@@ -103,14 +131,13 @@ int main() {
 
     cout << " \n-------/// PARTE 2 - ESTRUTURAÇÃO DOS DADOS ///-------\n\n";
 
-    cout << "2.1) Vamos criar um grafo de nós nessa parte, em que cada vértice tem\ncomo id uma substring. Para isso, vamos organizar tudo primeiro.\n";
-    cout << "Vamos ordenar o vetor de substrings por ordem alfabética. Isso vai faci-\nlitar a criação do grafo. \n\n";
+    cout << "2.1) Vamos ordenar o vetor de substrings por ordem alfabética. Isso vai facilitar a criação do grafo. \n\n";
 
     cout << " Deseja imprimir a versão ordenada? S/N\n >> ";
     cin >> op;
 
     sort(v.begin(), v.end(), compara_nodes);
-
+    
     // Corrigindo os ids de cada node:
     for(int i = 0; i < v.size(); i++) {
         v[i].id = i;
@@ -122,6 +149,26 @@ int main() {
         }
     }
 
+    cout <<"\n2.2) Agora vamos gerar um arquivo .txt com a lista de substrings\ne utilizá-lo para fazer a simulação no EP.\n";
+
+    cout << "Digite o nome do arquivo que deseja gerar: \n >> ";
+    string file_name; cin >> file_name;
+
+    ofstream arquivo(file_name);
+
+    if(arquivo.is_open()) {
+        arquivo << dna << endl;
+        arquivo << copy << endl;
+        arquivo << v.size() << endl;
+        for(node x: v) {
+            arquivo << x.key << endl;
+        }
+        arquivo.close();
+        cout << "Arquivo " << file_name << " gerado!\n";
+    }
+    else {
+        cout << "Não foi gerar o arquivo!\n";
+    }
     return 0;
 }
 
